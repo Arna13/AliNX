d1cb5ff9a3e06de3877c756935325c54050c7ba1
Arna13 <Arna13@users.noreply.github.com>
2020-04-14 19:37:29 +0200

    Hotfix

--- sd_files/cfw emunand.bat
+++ sd_files/cfw emunand.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Updating to only emunand hekate mode...
 cd bootloader
-mv hekate_ipl.ini hekate_ipl_sysnand.txt
-mv hekate_ipl_emunand.txt hekate_ipl.ini
+rename hekate_ipl.ini hekate_ipl_sysnand.txt
+rename hekate_ipl_emunand.txt hekate_ipl.ini
 echo Finished!
 pause
