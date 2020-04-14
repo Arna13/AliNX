17f7c1b8273f91e6006c0916f940bce3df97fd98
Arna13 <Arna13@users.noreply.github.com>
2020-04-14 21:06:49 +0200

    Deleted batch files

--- sd_files/cfw emunand.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Updating to only emunand hekate mode...
-cd bootloader
-rename hekate_ipl.ini hekate_ipl_sysnand.txt
-rename hekate_ipl_emunand.txt hekate_ipl.ini
-echo Finished!
-pause
--- sd_files/cfw sysnand.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Updating to sysnand + emunand hekate support...
-cd bootloader
-rename hekate_ipl.ini hekate_ipl_emunand.txt
-rename hekate_ipl_sysnand.txt hekate_ipl.ini
-echo Finished!
-pause
